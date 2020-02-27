9c3ea70d34590e05cfc981c1aee979c25bb71957
MD. SOHEL PARVEZ <s.parvez1971@gmail.com>
2020-02-27 17:07:30 +0600

    up

---
+++ unicode.r
@@ -0,0 +1,32 @@
+
+icons <- c("\U0001F334", "\U0001F332", "\U0001F38B", "\U0001F333", "\U0001F384")
+
+for(i in 0:length(icons)) {
+  temp = c()
+  vector = c()
+  idx<-0
+  for(icon in 0:length(icons)) {
+    randomNum <- floor(runif(1, min=0, max=4));
+    if(idx == 0) {
+        vector[idx] <- randomNum
+        #print(icons[randomNum])
+        temp[idx] <- icons[randomNum+1]
+    } else {
+      #print(randomNum)
+      #print(temp)
+      #print(randomNum)
+      while (is.element(randomNum, vector)){
+        randomNum <- floor(runif(1, min=0, max=5));
+      }
+      vector[idx] <- randomNum
+      #print(icons[randomNum])
+      temp[idx] <- icons[randomNum+1]
+      #print(pp[j])
+    }
+    idx<-idx+1
+  }
+  utf8::utf8_print(temp)
+}
+    
+    
+
